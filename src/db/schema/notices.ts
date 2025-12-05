adc0a38824e9845b2e4758a344f17c5b59109f40
chandandebsingha <web.dev.mi04.com>
2025-12-05 16:13:52 +0530

    pre approve and notices backed added and fixes

--- src/db/schema/notices.ts
+++ src/db/schema/notices.ts
@@ -1,11 +1,10 @@
 import {
-	pgTable,
-	serial,
-	integer,
-	varchar,
-	timestamp,
+    integer,
+    pgTable,
+    serial,
+    timestamp,
+    varchar,
 } from "drizzle-orm/pg-core";
-import { InferModel } from "drizzle-orm";
 import { users } from "./users";
 
 export const notices = pgTable("notices", {
@@ -15,7 +14,6 @@ export const notices = pgTable("notices", {
         .references(() => users.id),
     title: varchar("title", { length: 255 }).notNull(),
     content: varchar("content", { length: 2000 }).notNull(),
-    imageUrl: varchar("image_url", { length: 500 }),
     createdAt: timestamp("created_at", { withTimezone: true })
         .defaultNow()
         .notNull(),
